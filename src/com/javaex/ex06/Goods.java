63d12d45798b6a5775ec052e0a0b102af05de8a5
soyoungJin44 <wlsthdud0412@naver.com>
2024-07-02 15:00:48 +0900

    생성자-디폴트생성자부분 필기

--- src/com/javaex/ex06/Goods.java
+++ src/com/javaex/ex06/Goods.java
@@ -11,10 +11,9 @@ public class Goods {
 	}
 	//생성자의 주 기능은 ***메모리에 올리는게 주 업무*** 이기때문에 올리는 방식을 추가함.
 	public Goods(String name, int price) {
-		this.name = name;
-		this.price = price;
-	}
-	
+		this.name = name;						//생성자를 작성자가 의지대로 코드를 작성하게되면, 디폴트 생성자코드[public Goods]는 작성되지않는다.
+		this.price = price;						//(생성자 부분에 아무 코드도 작성하지않으면 그때는 디폴트 생성자코드가[public Goods] 작성된다.				
+	}											//***두가지 생성자방식을 동시에 쓰고 싶다면, 디폴트 생성자코드가[public Goods]를 직접 작성하면된다.
 	
 	//메서드-gs(getter,setter)
 	public String getName() {
