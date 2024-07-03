85a28e693cb031df7de8442a545adc621422671f
soyoungJin44 <wlsthdud0412@naver.com>
2024-07-03 12:33:23 +0900

    생성자 _ 자료형까지만 읽고 확인이된다.

--- src/com/javaex/ex04/Point.java
+++ src/com/javaex/ex04/Point.java
@@ -9,6 +9,20 @@ public class Point {
 	
 	//생성자
 	
+	public Point() {
+		
+	}
+	/*
+	public Point(int x) {
+		this.x = x;
+	}
+	
+	public Point(int y) {
+		this.y = y;
+	}
+	*/ // **위 두가지는 동일한모습으로 인식한다ㅏ. Point(int) (자료형까지만) 확인을 하기 때문에 x,y를 구분할수는 없다.
+	
+	
 	//메서드
 	//<좌표 받을곳>
 	public void setX(int x) {
@@ -26,6 +40,8 @@ public class Point {
 	public int getY() { //getter
 		return y;
 	}
+	
+	
 	
 	public void draw() {
 		System.out.println("점[x= " + getX() + "],점[y= " + getY() +"]를 그렸습니다.");
--- src/com/javaex/ex04/PointApp.java
+++ src/com/javaex/ex04/PointApp.java
@@ -16,6 +16,10 @@ public class PointApp {
 		B.setY(23);
 		
 		B.draw();
+		
+		
+		
+		
 	}
 
 }
